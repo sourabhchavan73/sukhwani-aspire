9fbc838a78fa4a818eae33ad530e7ed77f707973
Sourabh Chavan <sourabh@brandzgarage.com>
2021-08-07 15:23:06 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ $('#pills-tab li').click(function(){
 
   window.addEventListener("scroll", (event) => {
     var scroll = this.scrollY;
-    if  (scroll > 600){
+    if  (scroll > 10){
       $('#mynav').addClass('nav-colored');
       $('#mynav').removeClass('navbar-padding');
     }else {
